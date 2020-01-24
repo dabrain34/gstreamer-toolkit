3b78b6de49c5f7ae86e1cac2becf3721d3898be7
Stéphane Cerveau <scerveau@collabora.com>
2020-01-24 16:55:08 +0100

    gst-build: add helper scripts for cross compilation.

---
+++ gst-build-helper/cross-gst-uninstalled.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import platform
+import re
+import site
+import shutil
+import subprocess
+import sys
+import tempfile
+import pathlib
+
+from distutils.sysconfig import get_python_lib
+
+#from common import get_meson
+
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+
+# Use '_build' as the builddir instead of 'build'
+DEFAULT_BUILDDIR = os.path.join(SCRIPTDIR, 'gst-build-install')
+if not os.path.exists(DEFAULT_BUILDDIR):
+    DEFAULT_BUILDDIR = os.path.join(SCRIPTDIR, '_build')
+
+
+def listify(o):
+    if isinstance(o, str):
+        return [o]
+    if isinstance(o, list):
+        return o
+    raise AssertionError('Object {!r} must be a string or a list'.format(o))
+
+def stringify(o):
+    if isinstance(o, str):
+        return o
+    if isinstance(o, list):
+        if len(o) == 1:
+            return o[0]
+        raise AssertionError('Did not expect object {!r} to have more than one element'.format(o))
+    raise AssertionError('Object {!r} must be a string or a list'.format(o))
+
+def prepend_env_var(env, var, value):
+    env[var] = os.pathsep + value + os.pathsep + env.get(var, "")
+    env[var] = env[var].replace(os.pathsep + os.pathsep, os.pathsep).strip(os.pathsep)
+
+
+def get_subprocess_env(options):
+    env = os.environ.copy()
+    PREFIX_DIR = os.path.join(options.builddir, 'usr', 'local')
+    PREFIX_LOCAL_DIR = os.path.join(options.builddir, 'usr', 'local')
+    env["CURRENT_GST"] = os.path.normpath(SCRIPTDIR)
+    env["GST_VALIDATE_SCENARIOS_PATH"] = os.path.normpath(
+        "%s/subprojects/gst-devtools/validate/data/scenarios" % options.builddir)
+    env["GST_VALIDATE_PLUGIN_PATH"] = os.path.normpath(
+        "%s/subprojects/gst-devtools/validate/plugins" % options.builddir)
+    env["GST_VALIDATE_APPS_DIR"] = os.path.normpath(
+        "%s/subprojects/gst-editing-services/tests/validate" % options.builddir)
+    prepend_env_var(env, "PATH", os.path.normpath(
+        "%s/subprojects/gst-devtools/validate/tools" % options.builddir))
+    prepend_env_var(env, "PATH", os.path.join(SCRIPTDIR, 'meson'))
+    env["GST_VERSION"] = options.gst_version
+    env["GST_ENV"] = 'gst-' + options.gst_version
+
+    env["GST_REGISTRY"] = os.path.normpath(options.builddir + "/registry.dat")
+
+    sharedlib_reg = re.compile(r'\.so|\.dylib|\.dll')
+    typelib_reg = re.compile(r'.*\.typelib$')
+    pluginpath_reg = re.compile(r'lib.*' + re.escape(os.path.normpath('/gstreamer-1.0/')))
+
+    if os.name is 'nt':
+        lib_path_envvar = 'PATH'
+    elif platform.system() == 'Darwin':
+        lib_path_envvar = 'DYLD_LIBRARY_PATH'
+    else:
+        lib_path_envvar = 'LD_LIBRARY_PATH'
+
+    prepend_env_var(env, "GST_PLUGIN_PATH", os.path.join(PREFIX_LOCAL_DIR, 'lib',
+                                                        'gstreamer-1.0'))
+    prepend_env_var(env, "PATH", os.path.join(PREFIX_DIR, 'bin'))
+    
+    prepend_env_var(env, lib_path_envvar, os.path.join(options.builddir, 'lib'))    
+    prepend_env_var(env, lib_path_envvar, os.path.join(PREFIX_DIR, 'lib'))
+    prepend_env_var(env, lib_path_envvar, os.path.join(PREFIX_LOCAL_DIR, 'lib'))
+    
+    prepend_env_var(env, "GST_VALIDATE_SCENARIOS_PATH", os.path.join(
+        PREFIX_DIR, 'share', 'gstreamer-1.0', 'validate', 'scenarios'))
+    prepend_env_var(env, "GI_TYPELIB_PATH", os.path.join(PREFIX_DIR, 'lib',
+                                                         'lib', 'girepository-1.0'))
+    env["GST_OMX_CONFIG_DIR"] = os.path.join(PREFIX_DIR, 'etc', 'xdg')
+
+
+    return env
+
+# https://stackoverflow.com/questions/1871549/determine-if-python-is-running-inside-virtualenv
+def in_venv():
+    return (hasattr(sys, 'real_prefix') or
+            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+
+def python_env(options, unset_env=False):
+    """
+    Setup our overrides_hack.py as sitecustomize.py script in user
+    site-packages if unset_env=False, else unset, previously set
+    env.
+    """
+    subprojects_path = os.path.join(options.builddir, "subprojects")
+    gst_python_path = os.path.join(SCRIPTDIR, "subprojects", "gst-python")
+    if not os.path.exists(os.path.join(subprojects_path, "gst-python")) or \
+            not os.path.exists(gst_python_path):
+        return False
+
+    if in_venv ():
+        sitepackages = get_python_lib()
+    else:
+        sitepackages = site.getusersitepackages()
+
+    if not sitepackages:
+        return False
+
+    sitecustomize = os.path.join(sitepackages, "sitecustomize.py")
+    overrides_hack = os.path.join(gst_python_path, "testsuite", "overrides_hack.py")
+    mesonconfig = os.path.join(gst_python_path, "testsuite", "mesonconfig.py")
+    mesonconfig_link = os.path.join(sitepackages, "mesonconfig.py")
+
+    if not unset_env:
+        if os.path.exists(sitecustomize):
+            if os.path.realpath(sitecustomize) == overrides_hack:
+                print("Customize user site script already linked to the GStreamer one")
+                return False
+
+            old_sitecustomize = os.path.join(sitepackages,
+                                            "old.sitecustomize.gstuninstalled.py")
+            shutil.move(sitecustomize, old_sitecustomize)
+        elif not os.path.exists(sitepackages):
+            os.makedirs(sitepackages)
+
+        os.symlink(overrides_hack, sitecustomize)
+        os.symlink(mesonconfig, mesonconfig_link)
+        return os.path.realpath(sitecustomize) == overrides_hack
+    else:
+        if not os.path.realpath(sitecustomize) == overrides_hack:
+            return False
+
+        os.remove(sitecustomize)
+        os.remove (mesonconfig_link)
+        old_sitecustomize = os.path.join(sitepackages,
+                                            "old.sitecustomize.gstuninstalled.py")
+
+        if os.path.exists(old_sitecustomize):
+            shutil.move(old_sitecustomize, sitecustomize)
+
+        return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="gstreamer-uninstalled")
+
+    parser.add_argument("--builddir",
+                        default=DEFAULT_BUILDDIR,
+                        help="The meson build directory")
+    parser.add_argument("--srcdir",
+                        default=SCRIPTDIR,
+                        help="The top level source directory")
+    parser.add_argument("--gst-version", default="master",
+                        help="The GStreamer major version")
+    options, args = parser.parse_known_args()
+
+    if not os.path.exists(options.builddir):
+        print("GStreamer not built in %s\n\nBuild it and try again" %
+              options.builddir)
+        exit(1)
+
+    if not os.path.exists(options.srcdir):
+        print("The specified source dir does not exist" %
+              options.srcdir)
+        exit(1)
+
+    if not args:
+        if os.name is 'nt':
+            args = [os.environ.get("COMSPEC", r"C:\WINDOWS\system32\cmd.exe")]
+        else:
+            args = [os.environ.get("SHELL", os.path.realpath("/bin/sh"))]
+        if "bash" in args[0]:
+            bashrc = os.path.expanduser('~/.bashrc')
+            if os.path.exists(bashrc):
+                tmprc = tempfile.NamedTemporaryFile(mode='w')
+                with open(bashrc, 'r') as src:
+                    shutil.copyfileobj(src, tmprc)
+                tmprc.write('\nexport PS1="[gst-%s] $PS1"' % options.gst_version)
+                tmprc.flush()
+                # Let the GC remove the tmp file
+                args.append("--rcfile")
+                args.append(tmprc.name)
+    python_set = python_env(options)
+    try:
+        exit(subprocess.call(args, cwd=options.srcdir, close_fds=False,
+                             env=get_subprocess_env(options)))
+    except subprocess.CalledProcessError as e:
+        exit(e.returncode)
+    finally:
+        if python_set:
+            python_env(options, unset_env=True)
---
+++ gst-build-helper/generate-cross-file.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+
+import argparse
+import contextlib
+import json
+import os
+import platform
+import shlex
+import subprocess
+
+MESON_CROSS_FILE_TPL = \
+'''
+[host_machine]
+system = '{system}'
+cpu_family = '{cpu_family}'
+cpu = '{cpu}'
+endian = '{endian}'
+
+[properties]
+{extra_properties}
+
+[binaries]
+c = {CC}
+cpp = {CXX}
+ar = {AR}
+pkgconfig = 'pkg-config'
+{extra_binaries}
+'''
+
+SCRIPTDIR = os.path.normpath(os.path.dirname(__file__))
+
+def get_toolchain_prefix(options):
+    if options.toolchain_prefix != '':
+      return options.toolchain_prefix
+    prefix = ''
+    if options.cpu_family == "aarch64":
+        prefix = 'aarch64-linux-gnu-'
+    elif options.target_arch == "arm":
+        prefix = 'arm-linux-gnu-'
+    return prefix
+
+def get_cflags(options):
+    cflags = ' -Wall -g -O2'
+    if options.target_arch == "armv7":
+        cflags = ' -march=armv7-a '
+    cflags = cflags + ' ' + options.custom_cflags
+    return cflags
+
+def get_ldflags(options):
+    ldflags = ''
+    if options.sysroot != '':
+      ldflags = '-L{}/lib -Wl,-rpath-link={}/lib'.format(options.sysroot, options.sysroot)
+
+    ldflags = ldflags + ' ' + options.custom_ldflags
+    return ldflags
+
+def get_subprocess_env(options):
+    env = os.environ.copy()
+    prefix = get_toolchain_prefix(options)
+    cflags = get_cflags (options)
+    cxxflags = get_cflags (options)
+    ldflags = get_ldflags (options)
+    if options.use_clang:
+        env['CC'] = 'clang'
+        env['CXX'] = 'clang++'
+        env['CPP'] = 'clang'
+        prefix = ''
+    else:
+        env['CC'] = prefix + 'gcc'
+        env['CXX'] = prefix + 'g++'
+        env['CPP'] = prefix + 'cpp'
+
+    env['LD'] = prefix + 'ld'
+    env['STRIP'] = prefix + 'strip'
+    env['OBJCOPY'] = prefix + 'objcopy'
+    env['RANLIB'] = prefix + 'ranlib'
+    env['AS'] = prefix + 'as'
+    env['AR'] = prefix + 'ar'
+    env['NM'] = prefix + 'nm'
+
+    env['CFLAGS'] = cflags
+    env['CXXFLAGS'] = cxxflags
+    env['OBJCFLAGS'] = cflags
+    env['LDFLAGS'] = ldflags
+    if options.sysroot != '':
+      env['PKG_CONFIG_LIBDIR'] = '{}/lib/pkgconfig:{}/usr/share/pkgconfig'.format(options.sysroot, options.sysroot)
+    else:
+      env['PKG_CONFIG_LIBDIR'] = '__no_cross_sysroot__'
+
+    return env
+
+def _write_meson_cross_file (env, options):
+
+    cc = env['CC'].split()
+    cxx = env['CXX'].split()
+    ar = env['AR'].split()
+    strip = env.get('STRIP', '').split()
+    windres = env.get('WINDRES', '').split()
+
+    # We do not use cmake dependency files, speed up the build by disabling it
+    cross_binaries = {'cmake': ['false']}
+    if 'STRIP' in env:
+        cross_binaries['strip'] = env['STRIP'].split()
+    if 'WINDRES' in env:
+        cross_binaries['windres'] = env['WINDRES'].split()
+    if 'OBJC' in env:
+        cross_binaries['objc'] = env['OBJC'].split()
+    if 'OBJCXX' in env:
+        cross_binaries['objcpp'] = env['OBJCXX'].split()
+
+
+    # *FLAGS are only passed to the native compiler, so while
+    # cross-compiling we need to pass these through the cross file.
+    c_args = shlex.split(env.get('CFLAGS', ''))
+    cpp_args = shlex.split(env.get('CXXFLAGS', ''))
+    objc_args = shlex.split(env.get('OBJCFLAGS', ''))
+    objcpp_args = shlex.split(env.get('OBJCXXFLAGS', ''))
+    # Link args
+    c_link_args = shlex.split(env.get('LDFLAGS', ''))
+    cpp_link_args = c_link_args
+    if 'OBJLDFLAGS' in env:
+        objc_link_args = shlex.split(env['OBJLDFLAGS'])
+    else:
+        objc_link_args = c_link_args
+    objcpp_link_args = objc_link_args
+    
+    pkg_config_libdir = shlex.split(env.get('PKG_CONFIG_LIBDIR', ''))
+
+    # Operate on a copy of the recipe properties to avoid accumulating args
+    # from all archs when doing universal builds
+    cross_properties = {}#copy.deepcopy(self.meson_cross_properties)
+    for args in ('c_args', 'cpp_args', 'objc_args', 'objcpp_args',
+                 'c_link_args', 'cpp_link_args', 'objc_link_args',
+                 'objcpp_link_args', 'pkg_config_libdir'):
+        if args in cross_properties:
+            cross_properties[args] += locals()[args]
+        else:
+            cross_properties[args] = locals()[args]
+
+    extra_properties = ''
+    for k, v in cross_properties.items():
+        extra_properties += '{} = {}\n'.format(k, str(v))
+    if not options.no_include_sysroot:
+      extra_properties += '{} = \'{}\'\n'.format('sys_root', str(options.sysroot))
+
+    extra_binaries = ''
+    for k, v in cross_binaries.items():
+        extra_binaries += '{} = {}\n'.format(k, str(v))
+
+    # Create a cross-info file that tells Meson and GCC how to cross-compile
+    # this project
+    cross_file = os.path.join(options.cross_file)
+    contents = MESON_CROSS_FILE_TPL.format(
+            system=options.target_platform,
+            cpu=options.target_arch,
+            cpu_family=options.cpu_family,
+            # Assume all ARM sub-archs are in little endian mode
+            endian='little',
+            CC=cc,
+            CXX=cxx,
+            AR=ar,
+            extra_binaries=extra_binaries,
+            extra_properties=extra_properties)
+    with open(cross_file, 'w') as f:
+        f.write(contents)
+
+    return cross_file
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--cross-file", '-c',
+                        default='my-meson-cross-file.txt',
+                        help="The meson cross-file, default: 'my-meson-cross-file.txt'.")
+    parser.add_argument("--toolchain-prefix", '-t',
+                        default='',
+                        help="The toolchain prefix, default: ''.")
+    parser.add_argument("--custom-cflags", '-C',
+                        default='',
+                        help="The custom cflags from the toolchain, default: ''.")
+    parser.add_argument("--custom-ldflags", '-L',
+                        default='',
+                        help="The custom ldflags from the toolchain, default: ''.")
+    parser.add_argument("--sysroot", '-s',
+                        default='',
+                        help="The sysroot directory to use, default: ''.")
+    parser.add_argument("--target-platform", '-p',
+                        default='linux',
+                        help="The target platform to use, default: 'linux'.")
+    parser.add_argument("--target-arch", '-a',
+                        default='arm64',
+                        help="The target arch to use, default: 'aarch64'.")
+    parser.add_argument("--cpu-family", '-f',
+                        default='aarch64',
+                        help="The cpu family to use, default: 'arm64'.")
+    parser.add_argument("--no-include-sysroot",
+                        default=False,
+                        action='store_true',
+                        help="dot not include sysroot in the cross file.")
+    parser.add_argument("--use-clang",
+                        default=False,
+                        action='store_true',
+                        help="dot not include sysroot in the cross file.")
+
+    options = parser.parse_args()
+    
+    env = get_subprocess_env (options)
+
+    print("Creating meson cross file with file name '%s' and options:" % options.cross_file)
+    print("")
+    print("\tsysroot: %s" % options.sysroot)
+    print("\ttarget platform: %s" % options.target_platform)
+    print("\ttarget architecture: %s" % options.target_arch)
+    print("\tcpu family: %s" % options.cpu_family)
+    print("\ttoolchain-prefix: %s" % get_toolchain_prefix(options))
+    print("\tCFLAGS: %s" % get_cflags(options))
+    print("\tLDFLAGS: %s" % get_ldflags(options))
+    print("")
+
+    cross_file = _write_meson_cross_file (env, options)
+    print("The cross file has been written correctly. You can now run 'meson configure --cross-file=%s'" %  cross_file)
