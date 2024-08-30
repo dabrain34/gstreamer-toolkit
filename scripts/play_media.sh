9aa565625a9be2a718a04292b1c69bf232a7870b
Stéphane Cerveau <scerveau@igalia.com>
2024-08-30 17:35:06 +0200

    gstreamer: simplify the play-media script
    Remove flu

--- scripts/play_media.sh
+++ scripts/play_media.sh
@@ -6,6 +6,7 @@ then
 echo "Usage: $0 /path/file"
 exit 1
 fi
+
 FILENAME=$1
 
 
@@ -13,8 +14,8 @@ FILENAME=$1
 if [ -z $DEMUXER ]; then DEMUXER=qtdemux; fi
 if [ -z $PARSER ]; then PARSER=h264parse; fi
 if [ -z $OPTS ]; then OPTS=-v; fi
-if [ -z $VT_DEC ]; then VT_DEC=vtdec_h264; fi
-if [ -z $PIPEPLINE_TYPE ]; then PIPEPLINE_TYPE=fluvadec; fi
+if [ -z $DECODER ]; then PIPEPLINE_TYPE=vah264dec; fi
+if [ -z $PIPELINE_TYPE ]; then PIPEPLINE_TYPE=vah264dec; fi
 if [ -z $GST_LAUNCH ]; then GST_LAUNCH="gst-launch"; fi
 if [ -z $PLAYBIN_ELEMENT ]; then PLAYBIN_ELEMENT="playbin2"; fi
 if [ -z $SINK ]; then 
@@ -28,45 +29,28 @@ fi
 
 if [ "x$2" != "x" ]
 then
-PIPEPLINE_TYPE=$2
+PIPELINE_TYPE=$2
 fi
 
 if [ -z $GST_DEBUG ]; then
-export GST_DEBUG="*:2,vtdec*:9,fluvadec*:9, videodeco*:9"
+export GST_DEBUG="*:2,vtdec*:9, videodeco*:9"
 fi
 
 if [ -z $GST_DEBUG_FILE ]; then
-export GST_DEBUG_FILE="/tmp/$(basename $FILENAME)_$PIPEPLINE_TYPE.log"
+export GST_DEBUG_FILE="/tmp/$(basename $FILENAME)_$PIPELINE_TYPE.log"
 fi
 
 #SRC_OPTIONS="num-buffers=50"
 
 set -x
-case "$PIPEPLINE_TYPE" in
+case "$PIPELINE_TYPE" in
 
 playbin) echo $TYPE
     $GST_LAUNCH $OPTS $PLAYBIN_ELEMENT uri="file://$FILENAME"
     ;;
 
-$VT_DEC) echo $TYPE
-    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! $VT_DEC ! $SINK
-    ;;
-
-fluh264dec) echo $TYPE
-    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! fluh264dec ! $SINK
-    ;;
-
-fluvadec) echo $TYPE
-    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! fluvadec ! fluvaconvert ! $SINK
-    ;;
-
-fluvadec-fakesink) echo $TYPE
-    FAKESINK="fakesink sync=TRUE"
-    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! fluvadec ! $SINK
-    ;;
-fluvadec-png) echo $TYPE
-    SINK="ffmpegcolorspace ! pngenc snapshot=FALSE ! multifilesink location=/tmp/frame_%05d.png"
-    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! fluvadec ! $SINK
+full) echo $TYPE
+    $GST_LAUNCH $OPTS filesrc location="$FILENAME" $SRC_OPTIONS ! $DEMUXER ! $PARSER ! $DECODER ! $SINK
     ;;
 *) echo "Invalid option"
    ;;
